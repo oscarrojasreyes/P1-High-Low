8b94b4066c5e9b41f7b545b72ffd6ce1b4a521cd
oscarrojasreyes <73680454+oscarrojasreyes@users.noreply.github.com>
2021-04-14 14:19:07 +0800

    Create High-Low-Game-ver1.0.py
    Adding High Low Game version 1.0 for python.

---
+++ Games/High-Low-Game-ver1.0.py
@@ -0,0 +1,38 @@
+import random
+
+class HighLow:
+    """ROCK IT!!!"""
+    def __init__(self):
+        """Init Test"""
+        self.secretNumber = 0
+        self.tries = 5
+        self.guessed = False
+        self.PlayGame()
+
+    def PlayGame(self):
+        """Begins the game of High Low"""
+        self.secretNumber = random.randint(1,10)
+
+        print("Welcome to HIGH/LOW game!")
+        print("You have 5 tries to guess the number!!.")
+        print("\n")
+        
+        while not self.guessed and self.tries > 0:
+            
+            guess = int(input("What is your guess? "))
+            self.tries -= 1
+
+            if guess < self.secretNumber:
+                print("Too low!")
+            elif guess > self.secretNumber:
+                print("Too high!")
+            else:
+                print("Correct!")
+                self.guessed = True
+                
+        if not self.guessed:
+            print("Game over!")
+        else:
+            print("Congrats!")
+
+newGame = HighLow()
