f9441fa201ea8d1d0372d99065aa5b73fee8c9bf
BradleyT <20058303@tafe.wa.edu.au>
2021-04-21 14:18:00 +0800

    Added Difficulty Option
    User can now set the difficulty at the start of the game.
    Corrects display to show secret number after user has lost.

--- Games/High-Low-Game-ver1.0.py
+++ Games/High-Low-Game-ver1.0.py
@@ -7,35 +7,47 @@ class HighLow:
         """Init Test"""
         self.secretNumber = 0
         self.tries = 5
+        self.difficulty = 0
         self.guessed = False
 
-    def PlayGame(self):
+    def setdifficulty(self):
+        _a = [8, 5, 3, 1]
+        print("What difficulty would you like to play with?\n"
+              "[1] Easy - 8 guesses\n"
+              "[2] Medium - 5 guesses\n"
+              "[3] Hard - 3 guesses\n"
+              "[4] Insane - 1 guess")
+        while self.difficulty not in [1, 2, 3, 4]:
+            self.difficulty = int(input("Choice: "))
+        self.tries = _a[self.difficulty-1]
+
+    def playgame(self):
         """Begins the game of High Low"""
         self.secretNumber = random.randint(1,10)
 
-        print("Welcome to HIGH/LOW game!")
-        print("You have 5 tries to guess the number!!.")
-        print("\n")
+        print("Welcome to Higher / Lower game!")
+
+        self.setdifficulty()
         
         while not self.guessed and self.tries > 0:
-            
-            guess = int(input("What is your guess? "))
+            print("Try to guess the secret number...")
+            guess = int(input(f"You have {self.tries} more tries. What is your guess? "))
             self.tries -= 1
 
-            if guess < self.secretNumber:
+            if guess < self.secretNumber and self.tries > 0:
                 print("Too low!")
-            elif guess > self.secretNumber:
+            elif guess > self.secretNumber and self.tries > 0:
                 print("Too high!")
             else:
                 print("Correct!")
                 self.guessed = True
                 
         if not self.guessed:
-            print("Game over!")
+            print(f"Game over! The secret number was {self.secretNumber}")
         else:
             print("Congrats!")
 
 
 if __name__ == '__main__':
     newGame = HighLow()
-    newGame.PlayGame()
+    newGame.playgame()
