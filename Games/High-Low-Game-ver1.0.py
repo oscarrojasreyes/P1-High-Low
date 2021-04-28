eb687dc302668e7dec96cba39d3506462bfb5d0b
tea8j <github@t8j.monster>
2021-04-27 23:31:06 -0600

    added custom range and removed an error when putting a non-integer into the difficulty select

--- Games/High-Low-Game-ver1.0.py
+++ Games/High-Low-Game-ver1.0.py
@@ -3,11 +3,13 @@ import random
 
 class HighLow:
     """ROCK IT!!!"""
+
     def __init__(self):
         """Init Test"""
         self.secretNumber = 0
         self.tries = 5
         self.difficulty = 0
+        self.range = 0
         self.guessed = False
 
     def setdifficulty(self):
@@ -18,17 +20,34 @@ class HighLow:
               "[3] Hard - 3 guesses\n"
               "[4] Insane - 1 guess")
         while self.difficulty not in [1, 2, 3, 4]:
-            self.difficulty = int(input("Choice: "))
-        self.tries = _a[self.difficulty-1]
+            try:
+                self.difficulty = int(input("Choice: "))
+            except ValueError:
+                print("that's not a number, silly")
+        self.tries = _a[self.difficulty - 1]
+
+    def setrange(self):
+        while self.range == 0:
+            range = input("How many numbers do you want to guess from? ")
+            if range:
+                try:
+                    self.range = int(range)
+                except ValueError:
+                    print("that's not a number, silly")
+            else:
+                self.range = 10
 
     def playgame(self):
         """Begins the game of High Low"""
-        self.secretNumber = random.randint(1,10)
 
         print("Welcome to Higher / Lower game!")
 
         self.setdifficulty()
-        
+
+        self.setrange()
+
+        self.secretNumber = random.randint(1, self.range)
+
         while not self.guessed and self.tries > 0:
             print("Try to guess the secret number...")
             guess = int(input(f"You have {self.tries} more tries. What is your guess? "))
@@ -41,7 +60,7 @@ class HighLow:
             else:
                 print("Correct!")
                 self.guessed = True
-                
+
         if not self.guessed:
             print(f"Game over! The secret number was {self.secretNumber}")
         else:
